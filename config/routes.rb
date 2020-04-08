2599305f18b724fdba01d4a1e8bb45d3e9e91cf8
vagrant <vagrant@localhost.localdomain>
2020-04-08 01:33:27 +0000

    first-commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+
+
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+  devise_for :users, controllers: { registrations: 'users/registrations',
+  									 sessions: 'users/sessions' }
+
+  resources :books, only: [:create, :index, :show, :edit, :update]
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  resources :users, only: [:show, :index, :edit, :update]
+  resource :home, only: [:top, :about]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
